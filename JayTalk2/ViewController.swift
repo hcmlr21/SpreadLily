37445d789c34da1bb61e9f61a1c1ca317ee1d0df
Jacker <hcmlr21@naver.com>
2020-08-11 17:49:53 +0900

    first commit

---
+++ JayTalk2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JayTalk2
+//
+//  Created by Jkookoo on 2020/08/11.
+//  Copyright © 2020 Jkookoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
