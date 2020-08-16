faf0f4121cb27813955cfd99e843683d3c1c537a
Jacker <hcmlr21@naver.com>
2020-08-17 00:01:35 +0900

    add chat rooms view

--- JayTalk2/TabBar/ChatRoomsViewController.swift
+++ JayTalk2/TabBar/ChatRoomsViewController.swift
@@ -7,22 +7,41 @@
 //
 
 import UIKit
+import Firebase
+import Kingfisher
 
 class ChatRoomsViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
     // MARK: - ProPerties
     let cellIdentifier: String = "chatRoomCell"
-    var chatRoomUid: String?
+    var myUid: String?
+    var chatRoomUid: [String] = []
+    var destinationsUids: [[String]] = []
     var chatRooms: [ChatModel] = []
     
     // MARK: - Methods
+    func getChatRoomsList() {
+        Database.database().reference().child("chatRooms").queryOrdered(byChild: "users/" + self.myUid!).queryEqual(toValue: true).observeSingleEvent(of: .value, with: { (dataSnapShot) in
+            self.chatRooms.removeAll()
+            for item in dataSnapShot.children.allObjects as! [DataSnapshot] {
+                if let chatRoomDic = item.value as? [String:AnyObject] {
+                    let chatModel = ChatModel(JSON: chatRoomDic)
+                    self.chatRoomUid.append(item.key)
+                    self.chatRooms.append(chatModel!)
+                }
+            }
+            self.tableView.reloadData()
+        })
+    }
     
     // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
     
     // MARK: - IBActions
     
     // MARK: - Delegates And DataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return self.chatRooms.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -30,22 +49,80 @@ class ChatRoomsViewController: UIViewController,UITableViewDataSource, UITableVi
             return UITableViewCell()
         }
         
+        var usersUid: [String] = []
+        var destinationUid: String?
         
+        for user in self.chatRooms[indexPath.row].users {
+            if user.key != self.myUid {
+                destinationUid = user.key
+                usersUid.append(destinationUid!)
+            }
+        }
         
+        self.destinationsUids.append(usersUid)
+        
+        Database.database().reference().child("users").child(destinationUid!).observeSingleEvent(of: .value, with: { (dataSnapShot) in
+            let userModel = UserModel()
+            userModel.setValuesForKeys(dataSnapShot.value as! [String : AnyObject])
+
+            cell.titleLabel.text = userModel.userName
+
+            let imageUrl = URL(string: userModel.profileImageUrl!)
+            cell.profileImageView.layer.cornerRadius = cell.profileImageView.frame.size.width / 2
+            cell.profileImageView.clipsToBounds = true
+            cell.profileImageView.kf.setImage(with: imageUrl!)
+
+            if(self.chatRooms[indexPath.row].comments.keys.count == 0) {
+                cell.lastMessageLabel.text = ""
+                cell.timeStampLabel.text = ""
+                return
+            }
+
+            let lastMessageKey = self.chatRooms[indexPath.row].comments.keys.sorted() {$0 > $1}
+            let lastMessageInfo = self.chatRooms[indexPath.row].comments[lastMessageKey[0]]
+            cell.lastMessageLabel.text = lastMessageInfo?.message
+            cell.timeStampLabel.text = lastMessageInfo?.timeStamp?.todayTime
+        })
+
+
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
+        
+        let usersCount = self.chatRooms[indexPath.row].users.count
+        
+        if(usersCount > 2) {
+            
+        } else {
+            let chatRoomVC = self.storyboard?.instantiateViewController(identifier: "chatRoomViewController") as! ChatRoomViewController
+            chatRoomVC.destinationUid = self.destinationsUids[indexPath.row][0]
+            self.navigationController?.pushViewController(chatRoomVC, animated: true)
+        }
+        
+    }
+    
     // MARK: - Life Cycles
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.myUid = Auth.auth().currentUser?.uid
+        
+        self.getChatRoomsList()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        self.getChatRoomsList()
     }
 }
 
 class ChatRoomTableCell: UITableViewCell {
-    @IBOutlet weak var profileImage: UIImageView!
-    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var lastMessageLabel: UILabel!
     @IBOutlet weak var timeStampLabel: UILabel!
     
